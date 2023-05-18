018cbf695720111262bcd9c0321143578d277c27
Steven Hall <stehall@chbc.school.nz>
2023-05-18 12:46:59 +1200

    constructed program and decleared methods

---
+++ FarmingFeed/Sheep.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarmingFeed
+{
+    class Sheep
+    {
+        //Attributes and Fields
+        private string breed;
+        private float totalFeedConsumed;
+        private float costOfFeedConsumed;
+        private List<float> WeeklyFeedConsumed = new List<float>() { 0,0,0,0,0,0,0};
+
+        //Constructor - Constructs an object of this Class
+        public Sheep(string breed, float totalFeedConsumed, float costOfFeedConsumed)
+        {
+            this.breed = breed;
+            this.totalFeedConsumed = 0;
+            this.costOfFeedConsumed = 0;
+
+
+        }
+        //Calculates total feed consumed
+        public float FeedConsumed()
+        {
+            for (int feedIndex = 0; feedIndex < 7; feedIndex++)
+            {
+
+            }
+
+        }
+
+        //returns the value in the private totalFeedConsumed variable
+        public float GetFeed()
+        {
+            return totalFeedConsumed;
+        }
+
+        
+        //Calculates cost of feed consumed
+        public float WeeklyFeedCost()
+        {
+            return 1;
+        }
+        
+        //Summarises data for a sheep and displays it 
+        public string SheepInfo()
+        {
+            return "";
+        }
+        
+        // returns a string collating all the values stored in the private variables
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+    }
+}
---
+++ FarmingFeed/SheepManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarmingFeed
+{
+    class SheepManager
+    {
+        //Attributes
+        private string sheepID;
+        private List<string> sheepBreeds = new List<string>() { "Arapawa", "Perendale", "Romney", "coopworth" };
+        private float feedCost;
+        private float avgHealthyFeedConsumed = 0.5f;
+        private int sheepNum;
+        //Constructor - constructs an object of this class
+        public SheepManager()
+        {
+
+        }
+        
+        //Calculates weekly feed consumed from all sheep
+        public float CalculateWeeklyFeedConsumed()
+        {
+            return 1;
+        }
+
+        //Calculates total cost for feed from all feed consumed by sheep
+        public float CalculateWeeklyCost()
+        {
+            return 1;
+        }
+
+        //Checks if sheep is over eating or not eating enough feed
+        public bool CheckWeight()
+        {
+            return true;
+        }
+
+        //Generates a ID for each sheep
+        public string GenerateID()
+        {
+            return "";
+        }
+
+        //Adds breed not specified on the list that the user farms
+        public void AddBreed()
+        {
+
+        }
+
+        //Calculates cost of food per year consumed by sheep 
+        public float CalculateYearlyCost()
+        {
+            return 1;
+        }
+
+        //Returns a string collating all the values stored in the private variables
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
